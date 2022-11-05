3be319a1dc37b50873d94ea41544d5efc3aadc5f
ankit417 <acharya.ankit417@gmail.com>
2022-11-05 20:07:21 +0545

    db details

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,15 +1,15 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 
 const dbConn = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"prediction"
-})
+  host: "localhost",
+  user: "ankit",
+  password: "YetiWorld123@#",
+  database: "prediction",
+});
 
-dbConn.connect(function(error){
-    if(error) throw error;
-    console.log('Database connected successfully')
-})
+dbConn.connect(function (error) {
+  if (error) throw error;
+  console.log("Database connected successfully");
+});
 
 module.exports = dbConn;
