7eca8c30c30d99243cd41e8294b7d00200708fae
ankit417 <acharya.ankit417@gmail.com>
2022-11-10 08:13:50 +0545

    DB

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,18 +1,18 @@
 const mysql = require("mysql");
 
-const dbConn = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "prediction",
-});
-
 // const dbConn = mysql.createConnection({
 //   host: "localhost",
 //   user: "root",
-//   password: "YetiWorld123@#",
+//   password: "",
 //   database: "prediction",
 // });
+
+const dbConn = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "YetiWorld123@#",
+  database: "prediction",
+});
 
 dbConn.connect(function (error) {
   if (error) throw error;
